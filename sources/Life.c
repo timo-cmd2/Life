ecd1903fcc832c8bff14ff6b678ff670a5c97d49
Timo Sarkar <71646577+timo-cmd2@users.noreply.github.com>
2021-01-18 16:11:06 +0100

    Initial commit

---
+++ sources/Life.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+#define rows 30
+#define cols 80
+
+typedef int Board[rows][cols];
+
+void main() {
+
+}
